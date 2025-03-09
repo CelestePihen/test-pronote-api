c0a8a796c92fa1ec064c0916aff71cdccdb3998e
Cel <53144420+CelestePihen@users.noreply.github.com>
2025-03-09 18:03:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import pronotepy
 import datetime
+
+# if you're using Pronote with an ENT then you have to import it
 from pronotepy.ent import ent_hdf
 
-client = pronotepy.Client("https://0620052v.index-education.net/pronote/eleve.html", # lien du pronote du lycée
-                          username="", # nom d'utilisateur ENT
-                          password="", # mot de passe ENT
-                          ent=ent_hdf) # ça marche avec les cookies qu'il récupére
+client = pronotepy.Client("https://0620052v.index-education.net/pronote/eleve.html", # link of the pronote of your establishment
+                          username="", # ENT username
+                          password="", # ENT password
+                          ent=ent_hdf) # by passing the ENT, the dependency will look at your cookies of your web browser
 
 
 def main():
     if client.logged_in:
-        print(f'Client connecté ! Username: {client.username} | Password: {client.password}')
+        print(f'Client connected ! Username: {client.username} | Password: {client.password}')
 
-        periods = client.periods # Notes
+        periods = client.periods
 
         for period in periods:
+            # Grades
             for grade in period.grades:
-                print(f'Note de {grade.subject.name} : {grade.grade}/{grade.out_of}')
+                print(f'Name of the Grade {grade.subject.name} : {grade.grade}/{grade.out_of}')
 
-        # Devoirs
+        # Homeworks
         homework = client.homework(datetime.date.today())
         for home in homework:
             print(f'{home.subject.name} : {home.description}')
 
     else:
-        print("Connexion au Client échoué !")
+        print("Connection to the Client failed !")
 
 
 if __name__ == '__main__':
